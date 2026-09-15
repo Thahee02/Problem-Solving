31b04f6762b5b6f9925d4b57870786274ec9fb35
Thahee02 <developerthahee@gmail.com>
2026-09-15 19:13:17 +0530

    Day 09: Sliding Window

---
+++ Day 09/maximum_sum_of_k_consecutive_elements.py
@@ -0,0 +1,20 @@
+numbers = [2, 1, 5, 1, 3, 2]
+k = 3
+
+window_max = 0
+
+# first window
+for i in range(k):
+    window_max += numbers[i]
+
+maximum_sum = window_max
+
+# slide the window
+for i in range(k, len(numbers)):
+    window_max -= numbers[i-k] + numbers[i]
+    if window_max > maximum_sum:
+        maximum_sum = window_max
+
+print(maximum_sum)
+
+
